c9333b5b40e9566d6e0cf85f83d61dbc641baf6b
Richard T <richard.tambong04@gmail.com>
2023-08-02 19:37:46 +0800

    Update

---
+++ readme.md
@@ -0,0 +1,41 @@
+# Tic Tac Toe Game
+
+This is a simple implementation of the classic Tic Tac Toe game using React. Players take turns to mark 'X' or 'O' on a 3x3 grid, aiming to get three of their marks in a row, column, or diagonal.
+
+## Demo
+
+https://tic-tac-tic-tac.vercel.app
+
+## Features
+
+- Play against a friend locally.
+- Restart the game at any time.
+- Displays the current game status, including the winner.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/chardtmbng02/TicTacTicTac.git
+
+2. Navigate to the project directory.
+3. Install dependencies.
+    npm install
+
+
+## Usage
+
+1. Start the development server.
+    npm run start or npm start
+
+
+## Game Rules
+
+- Players take turns to click on an empty cell to place their mark ('X' or 'O').
+
+- The game ends when a player has three marks in a row, column, or diagonal, or when the board is full (a draw).
+
+- Clicking on an occupied cell or after the game is won will have no effect.
+
+- Click the "Restart" button to reset the game.
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
-import './Main.css'; // Create a CSS file for styling if desired
+import React, { useState } from "react";
+import "./Main.css"; // CSS styling import
 
+// Define the Main component
 const Main = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true);
+  const [board, setBoard] = useState(Array(9).fill(null)); // Initialize the game board state with an array of 9 null values
+  const [isXNext, setIsXNext] = useState(true); // Initialize the player's turn state (true for 'X', false for 'O')
 
+  // This function will handle the square click.
   const handleClick = (index) => {
+    // Check if the square is already occupied or if there is a winner.
     if (board[index] || calculateWinner(board)) {
       return;
     }
-
+    // Create a new copy of the board array and update the clicked square.
     const newBoard = [...board];
-    newBoard[index] = isXNext ? 'X' : 'O';
+    newBoard[index] = isXNext ? "X" : "O";
 
     setBoard(newBoard);
     setIsXNext(!isXNext);
   };
 
+  // Function to render a single square button.
   const renderSquare = (index) => {
     return (
       <button className="square" onClick={() => handleClick(index)}>
@@ -25,8 +29,11 @@ const Main = () => {
     );
   };
 
+  // This will calculate the winner.
   const winner = calculateWinner(board);
-  const status = winner ? `Winner: ${winner}` : `Next player: ${isXNext ? 'X' : 'O'}`;
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${isXNext ? "X" : "O"}`;
 
   return (
     <div className="tic-tac-toe">
@@ -59,11 +66,12 @@ const calculateWinner = (board) => {
   for (const line of winningLines) {
     const [a, b, c] = line;
     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return board[a];
+      return board[a]; // Return the winning player ('X' or 'O')
     }
   }
 
-  return null;
+  // Return null if there's no winner
+  return null; 
 };
 
 export default Main;
